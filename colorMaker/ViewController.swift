a949d70b922758f49f9a30a6464fb1002480a03e
Miroslav Mikovec <miroslav.mikovec@gmail.com>
2022-03-06 20:40:22 +0100

    version I.

--- colorMaker/ViewController.swift
+++ colorMaker/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    // via keyword @IBOutlet make them recognizable for storyboard
+    // need to add weak keyword? now w/o
+    
+    // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Actions
+    
+    
 
 }
 
