6588abf8871c303a2470a011b4cd90086377a400
Miroslav Mikovec <miroslav.mikovec@gmail.com>
2022-03-07 15:02:30 +0100

    version III.

--- colorMaker/ViewController.swift
+++ colorMaker/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     // via keyword @IBOutlet make them recognizable for storyboard
     // need to add weak keyword? now w/o
     
+    @IBOutlet var colorPalette: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     // MARK: Life Cycle
 
     override func viewDidLoad() {
@@ -23,7 +28,22 @@ class ViewController: UIViewController {
 
     // MARK: Actions
     
-    
-
+    @IBAction func colorMix() { // (_ sender: AnyObject)
+        
+        if self.redSlider == nil {
+            return
+        } // Make sure the program doesn't crash if the controls aren't connected, can skip
+        
+        // sliders give Float value between 0 and 1, but the UIColor object must be constructed with CGFloat values
+        // if you have a Float and you need a CGFloat you can use the CGFloat constructor to convert between them
+            // let f: Float = 0.5
+            // let cgf: CGFloat = CGFloat(f)
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorPalette.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 }
 
